56655b32b2a67c9a608864e64f4035751e27f2c0
Túlio Gerbassi <104569234+tulio148@users.noreply.github.com>
2024-05-31 14:27:52 +0800

    Update servo_class.py doctring

--- servo_class.py
+++ servo_class.py
@@ -2,21 +2,39 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 class Servo:
+    """
+    A class to control a servo motor using Raspberry Pi GPIO.
+
+    Attributes:
+    ----------
+    pin : int
+        The GPIO pin number to which the servo is connected.
+
+    Methods:
+    -------
+    set_angle(angle):
+        Sets the servo to the specified angle.
+    duty_cycle(angle):
+        Changes the PWM duty cycle to control the servo angle.
+    cleanup():
+        Stops the PWM and cleans up the GPIO settings.
+    """
+
     def __init__(self, pin):
         self.pin = pin
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.setmode(GPIO.BOARD) 
+        GPIO.setup(self.pin, GPIO.OUT) 
         self.pwm = GPIO.PWM(self.pin, 50) 
-        self.pwm.start(0)
+        self.pwm.start(0)  
+
     def set_angle(self, angle):
-        duty = angle 
-        # sleep(1)  
+        duty = angle  
+        sleep(0.1)
         
     def duty_cycle(self, angle):
-        self.pwm.ChangeDutyCycle(angle)
-        sleep(0.1)
+        self.pwm.ChangeDutyCycle(angle) 
+        sleep(0.1)  
 
     def cleanup(self):
-        self.pwm.stop()
-        GPIO.cleanup()
-        
+        self.pwm.stop()  
+        GPIO.cleanup()  
