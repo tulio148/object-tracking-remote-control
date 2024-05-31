5e849c8b476c13b97d5d0f90deb37a5d241bfc21
Túlio Gerbassi <104569234+tulio148@users.noreply.github.com>
2024-05-31 14:29:19 +0800

    Update main.py docstring and comments

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-
 from servo_class import Servo
 from serial_class import SerialClass
 
 if __name__ == "__main__":
-    servo1 =  Servo(32)
+    # Initialize the first servo on GPIO pin 32 and set its angle to 0
+    servo1 = Servo(32)
     servo1.set_angle(0)
 
-    servo2 =  Servo(33)
+    # Initialize the second servo on GPIO pin 33 and set its angle to 0
+    servo2 = Servo(33)
     servo2.set_angle(0)
 
+    # Initialize the serial connection on port '/dev/ttyACM0'
     serial_controller = SerialClass('/dev/ttyACM0')
     serial_controller.connect()
 
@@ -17,13 +19,15 @@ if __name__ == "__main__":
             s = serial_controller.readline()
             if s:
                 numbers = s.split(' ')
+                # Check if both parts are available and valid
                 if len(numbers) == 2 and numbers[0] and numbers[1]:
                     x = float(numbers[0])
                     y = float(numbers[1])
-                    print(x, y)
+                    # Set the duty cycle of the servos to the received values
                     servo1.duty_cycle(x)
                     servo2.duty_cycle(y)
     except KeyboardInterrupt:
+        # Cleanup the servos and disconnect the serial connection on interrupt
         servo1.cleanup()
         servo2.cleanup()
         serial_controller.disconnect()
